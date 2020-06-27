06ec683f6e6a02c3500d3c25ad81d75bd4882b82
David Baker Effendi <davidbakereffendi@hotmail.com>
2020-06-27 19:03:26 +0200

    :sparkles: Have some basic text highlighting from sample code

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 import { LintingDetails } from './domain/LintingModel';
-import { LintingProvider } from './provider/LintingProvider';
+
 const axios = require('axios').default;
 
 export function activate(context: vscode.ExtensionContext) {
@@ -34,14 +34,80 @@ export function activate(context: vscode.ExtensionContext) {
         console.debug('axios error' + e);
     }
 
-    const selector: vscode.DocumentFilter[] = [];
-    for (const language of ['gsql']) {
-        selector.push({ language, scheme: 'file' });
-        selector.push({ language, scheme: 'untitled' });
+    let timeout: NodeJS.Timer | undefined = undefined;
+
+    // create a decorator type that we use to decorate small numbers
+    const smallNumberDecorationType = vscode.window.createTextEditorDecorationType({
+        borderWidth: '1px',
+        borderStyle: 'solid',
+        overviewRulerColor: 'blue',
+        overviewRulerLane: vscode.OverviewRulerLane.Right,
+        light: {
+            // this color will be used in light color themes
+            borderColor: 'darkblue'
+        },
+        dark: {
+            // this color will be used in dark color themes
+            borderColor: 'lightblue'
+        }
+    });
+
+    // create a decorator type that we use to decorate large numbers
+    const largeNumberDecorationType = vscode.window.createTextEditorDecorationType({
+        cursor: 'underline',
+        // use a themable color. See package.json for the declaration and default values.
+        backgroundColor: { id: 'myextension.largeNumberBackground' }
+    });
+
+    function updateDecorations() {
+        if (!activeEditor) {
+            return;
+        }
+        const regEx = /\d+/g;
+        const text = activeEditor.document.getText();
+        const smallNumbers: vscode.DecorationOptions[] = [];
+        const largeNumbers: vscode.DecorationOptions[] = [];
+        let match;
+        while ((match = regEx.exec(text))) {
+            const startPos = activeEditor.document.positionAt(match.index);
+            const endPos = activeEditor.document.positionAt(match.index + match[0].length);
+            const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: 'Number **' + match[0] + '**' };
+            if (match[0].length < 3) {
+                smallNumbers.push(decoration);
+            } else {
+                largeNumbers.push(decoration);
+            }
+        }
+        activeEditor.setDecorations(smallNumberDecorationType, smallNumbers);
+        activeEditor.setDecorations(largeNumberDecorationType, largeNumbers);
     }
 
-    context.subscriptions.push(
-        vscode.languages.registerCodeActionsProvider(selector, new LintingProvider(), LintingProvider.metadata));
+    function triggerUpdateDecorations() {
+        if (timeout) {
+            clearTimeout(timeout);
+            timeout = undefined;
+        }
+        timeout = setTimeout(updateDecorations, 500);
+    }
+
+    let activeEditor = vscode.window.activeTextEditor;
+
+    if (activeEditor) {
+        triggerUpdateDecorations();
+    }
+
+    vscode.window.onDidChangeActiveTextEditor(editor => {
+        activeEditor = editor;
+        if (editor) {
+            triggerUpdateDecorations();
+        }
+    }, null, context.subscriptions);
+
+    vscode.workspace.onDidChangeTextDocument(event => {
+        if (activeEditor && event.document === activeEditor.document) {
+            triggerUpdateDecorations();
+        }
+    }, null, context.subscriptions);
 }
 
 /**
